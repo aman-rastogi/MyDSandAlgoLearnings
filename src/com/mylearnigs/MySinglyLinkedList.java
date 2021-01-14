e56cd3a6b3b24d92aa7a53b8ca86b6c7f1e1e620
Aman Rastogi <rastogiaman690@gmail.com>
2021-01-14 08:50:16 -0500

    Initial Commit + Single LinkedList Baseline

---
+++ src/com/mylearnigs/MySinglyLinkedList.java
@@ -0,0 +1,30 @@
+package com.mylearnigs;
+
+public class MySinglyLinkedList {
+    int value;
+    MySinglyLinkedList next;
+
+    public MySinglyLinkedList(int value){
+        this.value = value;
+    }
+
+    public Integer get(int index){
+        return null;
+    }
+
+    public void addAtHead(int value){
+
+    }
+
+    public void addAtTail(int value){
+
+    }
+
+    public void addAtIndex(int index, int value){
+
+    }
+
+    public void deleteAtIndex(int index){
+
+    }
+}
---
+++ src/com/mylearnigs/TestMain.java
@@ -0,0 +1,7 @@
+package com.mylearnigs;
+
+public class TestMain {
+    public static void main(String[] args){
+        MySinglyLinkedList ll = new MySinglyLinkedList(10);
+    }
+}
